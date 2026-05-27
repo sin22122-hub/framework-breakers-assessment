ae6d060fedb2ba30614f2d7b71d6255aaf6e5c45
sin22122-hub <sin22122@gmail.com>
2026-05-28 02:35:05 +0800

    Delete src directory

--- src/components/ui/badge.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Badge({ className = '', children, ...props }) {
-  return (
-    <span className={`inline-flex items-center rounded-full ${className}`} {...props}>
-      {children}
-    </span>
-  )
-}
