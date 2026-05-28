fa4e3b8e1a57573713688ae9ee74a49da74db573
sin22122-hub <sin22122@gmail.com>
2026-05-28 15:33:53 +0800

    Delete src directory

--- src/components/ui/badge.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Badge({ className = "", ...props }) {
-  return <span className={`inline-flex items-center rounded-full ${className}`} {...props} />;
-}
--- src/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ className = "", ...props }) {
-  return <div className={`rounded-2xl border ${className}`} {...props} />;
-}
-
-export function CardContent({ className = "", ...props }) {
-  return <div className={className} {...props} />;
-}
